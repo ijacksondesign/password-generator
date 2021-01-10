9a6c38ad9489ac7ab98a044113350bb944abeb65
Ian Jackson <ijacksondesign@gmail.com>
2021-01-10 10:48:20 -0500

    fixed the bug where numbers were only being randomly generated one time and wrote README file

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# password-generator
+# Password Generator
+
+## Table of Contents
+* [Description](#description)
+* [The Challenge](#challenge)
+* [The Criteria](#criteria)
+* [Final Result](#results)
+* [Reflection](#reflection)
+
+
+## Description <a name="description"></a>
+This password generator was created as the third assigment for my coding Bootcamp and was the first project involving JavaScript.
+
+<img src="./screenshots/password-generator-screenshot.jpg"> 
+<img src="./screenshots/password-generator-screenshot2.jpg">
+
+## The Challenge <a name="challenge"></a>
+Create an interactive password generator to create secure passwords based on user input.
+
+## The Criteria <a name="criteria"></a>
+1. When "generate password" button is clicked, the user is presented with a series of prompts for password criteria to select
+1. User chooses to have at least 8 and no more than 128 characters
+1. User can choose between having lowercase, uppercase, numeric, and/or special characters
+1. The user input is validated to make sure they selected at least one of the above criteria
+1. Once the criteria is met, a randomly generated password is display on screen
+
+## Final Result <a name="results"></a>
+The final generator can be viewed at [https://ianjacksondesign.com/password-generator/](https://ianjacksondesign.com/password-generator/)
+
+## Reflection <a name="reflection"></a>
+This was the hardest project yet. I already had HTML and CSS knowledge coming in to this Bootcamp so the first couple of lessons were pretty familiar. I had studied JavaScript in the past as well and have what I consider the most basic of understandings of it, but have always struggled with it. This was no different. 
+
+I started out by writing out psuedocode to break down what I wanted the script to do. Once I already started rewritting my code though, I think I restarted a minimum of 3 times trying to figure out how to get everything to work. 
+
+I found myself getting really hung up on the fact that I knew the code I was writing wasn't the best code and that while it was working and doing what I wanted it to do, it was just extremely long and clunky. Even searching around and looking at stackoverflow and other forums and seeing people doing similar projects, I was seeing a lot of objects and functions that I couldn't quite wrap my head around. 
+
+In the end I have a working generator that hits all the required assignment criteria, and while I know the code could be better, I'm still proud I was able to write it myself. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,53 @@
 // Assignment code here
 
+
 // function for password length
 var getPassLength = function() {
+  // creates an empty variable for the password length
   var passLength = "";
 
+  // asks users to input how long they want their password to be
   while (passLength === "" || passLength === null) {
     passLength = prompt("How long would you like your password? Please enter a number between 8 and 128.");
 
+    // checks to make sure user input fits length criteria
     if (passLength >= 8 && passLength <= 128) {
       return passLength;
     } else {
+        // alerts user that they input an invalid answer      
         alert("Invalid input.");
+        
+        // runs function again for user to input a valid response
         getPassLength();
       }
   }
 };
 
+// function to output random numbers between 0 and the specified number
 var randomNumber = function(num) {
   var value = Math.floor(Math.random() * num);
   return value;
 };
 
+// variables for the characters that can be used in the password
+var lowerCase = "abcdefghiklmnopqrstuvwxyz";
+var upperCase = lowerCase.toUpperCase();
+var numeric = "0123456789"
+var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+// function to generates password
 var generatePassword = function() {
   
+  // calls the getPassLength function to get the password length from user
   passLength = getPassLength();
 
+  // series of confirms to get requested password parameters from user
   var passLowerCase = confirm("Do you want to use lowercase letters?");
   var passUpperCase = confirm("Do you want to use uppercase letters?");
   var passNumeric = confirm("Do you want to use numbers?");
   var passSpecial = confirm("Do you want to use special characters?");
 
+  // if user says no to all parameters, tells them they need to pick at least one, and asks them again
   while (passLowerCase === false && passUpperCase === false && passNumeric === false && passSpecial === false) {
     alert("You must selection at least one of the parameters: LOWERCASE, UPPERCASE, NUMBERS, or SPECIAL CHARACTERS.")
     passLowerCase = confirm("Do you want to use lowercase letters?");
@@ -38,13 +56,13 @@ var generatePassword = function() {
     passSpecial = confirm("Do you want to use special characters?");
   }
 
+  // empty password variable
   var password = "";
-  var lowerCase = "abcdefghiklmnopqrstuvwxyz";
-  var upperCase = lowerCase.toUpperCase();
-  var numeric = randomNumber(9);
-  var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
   
+  // generates the password. runs until the password is the user's requested length
   for (let i = 0; i < passLength; i++) {
+    
+    // if user wants all password criteria
     if (passLowerCase === true && passUpperCase === true && passNumeric === true && passSpecial === true) {
       switch (randomNumber(4)) {
         case 0:
@@ -54,12 +72,13 @@ var generatePassword = function() {
           password += upperCase[Math.floor(Math.random() * upperCase.length)];
           break;
         case 2:
-          password += numeric;
+          password += numeric[Math.floor(Math.random() * numeric.length)];
           break;
         case 3:
           password += specialChar[Math.floor(Math.random() * specialChar.length)];
       }
-    }  
+    }
+    // if user wants just lower, upper, and numbers criteria  
     else if (passLowerCase === true && passUpperCase === true && passNumeric === true) {
       switch (randomNumber(3)) {
         case 0:
@@ -69,9 +88,10 @@ var generatePassword = function() {
           password += upperCase[Math.floor(Math.random() * upperCase.length)];
           break;
         case 2:
-          password += numeric;
+          password += numeric[Math.floor(Math.random() * numeric.length)];
       }
     }
+    // if user wants just lower, upper, and special chars
     else if (passLowerCase === true && passUpperCase === true && passSpecial === true) {
       switch (randomNumber(3)) {
         case 0:
@@ -83,31 +103,34 @@ var generatePassword = function() {
         case 2:
           password += specialChar[Math.floor(Math.random() * specialChar.length)];
       }
-    }  
+    }
+    // if user wants just lower, numbers, and special chars  
     else if (passLowerCase === true && passNumeric === true && passSpecial === true) {
       switch (randomNumber(3)) {
         case 0:
           password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
           break;
         case 1:
-          password += numeric;
+          password += numeric[Math.floor(Math.random() * numeric.length)];
           break;
         case 2:
           password += specialChar[Math.floor(Math.random() * specialChar.length)];
       }
     }
+    // if user wants just upper, numbers, and special chars
     else if (passUpperCase === true && passNumeric === true && passSpecial === true) {
       switch (randomNumber(3)) {
         case 0:
           password += upperCase[Math.floor(Math.random() * upperCase.length)];
           break;
         case 1:
-          password += numeric;
+          password += numeric[Math.floor(Math.random() * numeric.length)];
           break;
         case 2:
           password += specialChar[Math.floor(Math.random() * specialChar.length)];
       }
     }
+    // if user wants just lower and upper
     else if (passLowerCase === true && passUpperCase === true) {
       switch (randomNumber(2)) {
         case 0:
@@ -117,15 +140,17 @@ var generatePassword = function() {
           password += upperCase[Math.floor(Math.random() * upperCase.length)];
       }
     }
+    // if user wants just lower and numbers
     else if (passLowerCase === true && passNumeric === true) {
       switch (randomNumber(2)) {
         case 0:
           password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
           break;
         case 1:
-          password += numeric;
+          password += numeric[Math.floor(Math.random() * numeric.length)];
       }
     }
+    // if user wants just lower and special chars
     else if (passLowerCase === true && passSpecial === true) {
       switch (randomNumber(2)) {
         case 0:
@@ -135,15 +160,17 @@ var generatePassword = function() {
           password += specialChar[Math.floor(Math.random() * specialChar.length)];
       }
     }
+    // if user wants just upper and numbers
     else if (passUpperCase === true && passNumeric === true) {
       switch (randomNumber(2)) {
         case 0:
           password += upperCase[Math.floor(Math.random() * upperCase.length)];
           break;
         case 1:
-          password += numeric;
+          password += numeric[Math.floor(Math.random() * numeric.length)];
       }
     }
+    // if user wants just upper and special
     else if (passUpperCase === true && passSpecial === true) {
       switch (randomNumber(2)) {
         case 0:
@@ -153,30 +180,32 @@ var generatePassword = function() {
           password += specialChar[Math.floor(Math.random() * specialChar.length)];
       }
     }
+    // if user wants just numbers and special chars
     else if (passNumeric === true && passSpecial === true) {
       switch (randomNumber(2)) {
         case 0:
-          password += numeric;
+          password += numeric[Math.floor(Math.random() * numeric.length)];
           break;
         case 1:
           password += specialChar[Math.floor(Math.random() * specialChar.length)];
       }
     }
+    // if user wants just lower
     else if (passLowerCase === true) {
       password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     }
+    // if user wants just upper
     else if (passUpperCase === true) {
       password += upperCase[Math.floor(Math.random() * lowerCase.length)];
     }
+    // if user wants just numbers
     else if (passNumeric === true) {
-      password += numeric;
+      password += numeric[Math.floor(Math.random() * numeric.length)];
     }
+    // if user wants just special chars
     else {
       password += specialChar[Math.floor(Math.random() * specialChar.length)];
-    }
-    
-    
-    
+    } 
   }
   return password;
 };
